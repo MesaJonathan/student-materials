3eef1aff1c152102c56ea1c3ae123160b3a55260
Jonathan Mesa <mesajonathan@ufl.edu>
2024-01-12 17:21:42 -0500

    started ex0

--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
@@ -3,4 +3,70 @@
 // LinkedList class should go in the "ufl_cap4053::fundamentals" namespace!
 namespace ufl_cap4053 { namespace fundamentals {
 
+template <class T>
+
+class LinkedList<T>{
+    pulbic LinkedList<T>(){
+        // MAKE SURE THE SIT STILL PUNISHMENT ISNT FUCKING UP THE DONE
+    }
+
+    public Iterator begin() const{
+
+    }
+
+    public Iterator end() const{
+
+    }
+
+    public bool isEmpty() const{
+
+    }
+
+    public T getFront() const{
+
+    }
+
+    public T getBack() const{
+        
+    }
+
+    public void enqueue(T element){
+
+    }
+
+    public void dequeue(){
+
+    }
+
+    public void pop(){
+
+    }
+
+    public bool contains(T element) const{
+
+    }
+
+    public void remove(T element){
+
+    }
+};
+
+class LinkedList<T>::Iterator{
+    public T opeator*() const{
+
+    }
+
+    public Iterator& operator++(){
+
+    }
+
+    public bool opeator==(Iterator const& rhs){
+
+    }
+
+    public bool operator!=(Iterator const& rhs){
+
+    }
+};
+
 } }  // namespace ufl_cap4053::fundamentals
