c4915ab7559dd9aba176efc62328e3e52b3da5e8
Jonathan Mesa <mesajonathan@ufl.edu>
2024-01-22 19:11:20 -0500

    gonna switch to mf vs now

--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
@@ -1,72 +1,164 @@
-
-
 // LinkedList class should go in the "ufl_cap4053::fundamentals" namespace!
 namespace ufl_cap4053 { namespace fundamentals {
 
-template <class T>
+template <typename T>
 
-class LinkedList<T>{
-    pulbic LinkedList<T>(){
-        // MAKE SURE THE SIT STILL PUNISHMENT ISNT FUCKING UP THE DONE
-    }
+class LinkedList{
+    private: 
+        struct Node{
+            T data;
+            Node* prev;
+            Node* next;
 
-    public Iterator begin() const{
+            Node(T data, Node* prev, Node* next){
+                this->data = data;
+                this->prev = prev;
+                this->next = next;
+            }
+        };
 
-    }
+        Node* head;
+        Node* tail;
 
-    public Iterator end() const{
+    public:
+        class Iterator{
+            private:
+                Node* curr;
 
-    }
+            public:
+                Iterator(Node* node){
+                    curr = node;
+                }
 
-    public bool isEmpty() const{
+                T opeator*() const{
+                    return *node.data;
+                }
 
-    }
+                Iterator& operator++(){
+                    curr = curr->next;
+                    return this;
+                }
 
-    public T getFront() const{
+                bool opeator==(Iterator const& rhs){
+                    if(curr == rhs.curr){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
 
-    }
+                bool operator!=(Iterator const& rhs){
+                    if(curr != rhs.curr){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+        };
 
-    public T getBack() const{
-        
-    }
+        LinkedList<T>(){
+            head = NULL;
+            tail = NULL;
+        }
 
-    public void enqueue(T element){
+        Iterator begin() const{
+            return Iterator(head);
+        }
 
-    }
+        Iterator end() const{
+            return Iterator(tail->next);
+        }
 
-    public void dequeue(){
+        bool isEmpty() const{
+            if (head == NULL){
+                return true;
+            }else{
+                return false;
+            }
+        }
 
-    }
+        T getFront() const{
+            return head;
+        }
 
-    public void pop(){
+        T getBack() const{
+            return tail;
+        }
 
-    }
+        void enqueue(T element){
+            //empty case
+            if(head == NULL && tail == NULL){
+                Node newNode = new Node(element, NULL, NULL);
+                head = &NewNode;
+                tail = &NewNode;
+            }else{  //non-empty case
+                // create new node with current tail as its prev val and NULL as its next val
+                Node newNode = new Node(element, tail, NULL);
 
-    public bool contains(T element) const{
+                //set new node as current tail's next
+                tail->next = &newNode;
 
-    }
+                //make new node the new tail 
+                tail = &NewNode;
+            }
+        }
 
-    public void remove(T element){
+        void dequeue(){
+            Node* newHead = head->next;
+            newHead->prev = NULL;
 
-    }
-};
+            // delete the node stored in head
+            delete head;
+            
+            head = newHead;
+        }
 
-class LinkedList<T>::Iterator{
-    public T opeator*() const{
+        void pop(){
+            Node* temp = tail;
+            Node* newTail = tail.prev;
+            newTail->next = NULL;
 
-    }
+            // delete the node stored in tail
+            delete tail;
+            
+            tail = newTail;
+        }
 
-    public Iterator& operator++(){
+        void clear(){
+            Node* it = head;
+            while (it != NULL){
+                Node* n =  it->next;
+                
+                // here add the code that deletes the node it points too
+                delete it;
 
-    }
+                it = n;
+            }
+        }
 
-    public bool opeator==(Iterator const& rhs){
+        bool contains(T element) const{
+            result = false;
 
-    }
+            Node* it = head;
+            while (it != NULL){
+                if (*it.data == element){
+                    result = true;
+                    break;
+                }
+            }
 
-    public bool operator!=(Iterator const& rhs){
+            return result;
+        }
 
-    }
+        void remove(T element){
+            Node* it = head;
+            while (it != NULL){
+                if (*it.data == element){
+                    //here add the code to delete it
+                    delete it;
+                }
+            }
+        }
 };
 
 } }  // namespace ufl_cap4053::fundamentals
